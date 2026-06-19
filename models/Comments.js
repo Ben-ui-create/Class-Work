51254f81c5331466d0d90a621738fbd033fece99
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-06-19 20:06:48 +0400

    added models/Comments.js logic

---
+++ models/Comments.js
@@ -0,0 +1,71 @@
+import {DataTypes, Model} from 'sequelize';
+import db from '../clients/db.sequelize.js';
+
+import Users from './Users.js';
+import Articles from './Articles.js';
+
+class Comments extends Model {
+}
+
+Comments.init({
+  id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true
+  },
+
+  body: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+}, {
+  sequelize: db,
+  modelName: 'comments',
+  tableName: 'comments',
+  timestamps: true,
+});
+
+Users.hasMany(Articles,
+  {
+    as: 'articles',
+    foreignKey: 'authorId'
+  }
+);
+
+Articles.belongsTo(Users,
+  {
+    as: 'author',
+    foreignKey: 'authorId'
+  }
+);
+
+Articles.hasMany(Comments,
+  {
+    as: 'comments',
+    foreignKey: 'articleId'
+  }
+);
+
+Comments.belongsTo(Articles,
+  {
+    as: 'article',
+    foreignKey: 'articleId'
+  }
+);
+
+Users.hasMany(Comments,
+  {
+    as: 'comments',
+    foreignKey: 'userId'
+  }
+);
+
+Comments.belongsTo(Users,
+  {
+    as: 'author',
+    foreignKey: 'userId'
+  }
+);
+
+export default Comments;
