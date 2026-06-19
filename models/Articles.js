361853c4fc6f8cd58563343128493b5c49dbdff1
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-06-19 20:02:54 +0400

    added models/Articles.js logic

---
+++ models/Articles.js
@@ -0,0 +1,34 @@
+import {DataTypes, Model} from 'sequelize';
+import db from '../clients/db.sequelize.js';
+
+class Articles extends Model {
+}
+
+Articles.init({
+  id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true
+  },
+
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  body: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+
+  cover: {
+    type: DataTypes.STRING
+  },
+}, {
+  sequelize: db,
+  modelName: 'articles',
+  tableName: 'articles',
+  timestamps: true,
+});
+
+export default Articles;
