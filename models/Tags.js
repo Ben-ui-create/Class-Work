637b059f3236541216e57dfe5132d3414be78fd2
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-06-19 20:08:10 +0400

    added models/Tags.js logic

---
+++ models/Tags.js
@@ -0,0 +1,27 @@
+import {DataTypes, Model} from 'sequelize';
+import db from '../clients/db.sequelize.js';
+
+class Tags extends Model {
+}
+
+Tags.init({
+  id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true
+  },
+
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+
+}, {
+  sequelize: db,
+  modelName: 'tags',
+  tableName: 'tags',
+  timestamps: true,
+});
+
+export default Tags;
