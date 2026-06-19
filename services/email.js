7b636d44960db85a0d4b7054f18fc60871c5bfc2
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-06-19 19:50:23 +0400

    added services/email.js logic

---
+++ services/email.js
@@ -0,0 +1,38 @@
+import nodemailer from "nodemailer";
+import ejs from "ejs";
+import path from "path";
+import _ from "lodash";
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: 'augustine.strosin78@ethereal.email',
+    pass: 'PEAuNV6GQydDQ3rj93'
+  }
+});
+
+export default async function ({ to, subject, template, data, attachments = null }) {
+  try {
+    const filePath = path.resolve('views/email', template + '.ejs');
+    const html = await ejs.renderFile(filePath, { data });
+
+    const payload = {
+      from: '"Example Team" <team@example.com>',
+      to,
+      subject,
+      html,
+    }
+
+    if (!_.isEmpty(attachments)) {
+      payload.attachments = attachments;
+    }
+
+    const info = await transporter.sendMail(payload);
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (err) {
+    console.error("Error while sending mail:", err);
+  }
+}
